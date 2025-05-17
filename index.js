099978aff3860c171be56251513351ddc7e3cbee
main <claudia.raphael73@gmail.com>
2025-05-17 14:46:05 -0300

    create function to place bet and number of lines to bet on

--- index.js
+++ index.js
@@ -28,12 +28,50 @@ const deposit = () => {
             return numberDeposit;
         }   
     }
+};
+
+// numberBet > balance / lines means that the user cannot bet more than they have
+// balance / lines means that for each line, the user can bet that much
+const getBet = (balance, lines) => {
+    while (true) {
+        const bet = prompt("Place your bet: ");
+        const numberBet = parseFloat(bet); // converts str to float
+        if (isNaN(numberBet) || numberBet > balance / lines || numberBet <= 0) {
+            console.log("Invalid bet amount, try again");
+        } else {
+            return numberBet;
+        }
+    }
 }
-   
-const depositAmount = deposit(); 
+
+const getNumberOfLines = () => {
+    while (true) {
+        const lines = prompt("Enter the number of lines you want to bet on (1-3): ");
+        const numberLines = parseInt(lines); // converts str to integer
+
+        if (isNaN(numberLines) || numberLines <= 0 || numberLines > 3) {
+            console.log("invalid number of lines, try again");
+        } else {
+            return numberLines;
+        }
+    }
+}
+// console exhibition:
+
+
+
+
+
 console.log('You have deposited: $' + depositAmount)
+console.log('You have chosen ' + numberLines + ' lines to bet on.')
+console.log('You have chosen to bet $' + numberBet + ' on each line.')
 
 
+// functions calling
+const depositAmount = deposit();
+const numberLines = getNumberOfLines();
+const numberBet = getBet();
+let balance = deposit();
 
 // win: when you match 3 symbols in a row
 // You can win on all 3 rows
